cae66a58dda8fd05df00082114cbdf0a132f604a
Artem <artyom.balamutyuk1@gmail.com>
2020-10-04 15:16:11 +0300

    Add sorting algorithm

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,20 +1,57 @@
-// BinarySearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+int searchBinary(int numbers[], int left, int right, int key)
+{
+	int midd = 0;
+	while (1)
+	{
+		midd = (left + right) / 2;
+
+		if (key < numbers[midd])
+		{
+			right = midd - 1;
+		}
+		else if (key > numbers[midd])
+		{
+			left = midd + 1;
+		}
+		else
+		{
+			return midd;
+		}
+			
+
+		if (left > right)
+		{
+			return -1;
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int SIZE = 12;
+	int numbers[SIZE] = {};
+	for (int index = 0; index < SIZE; index++)
+	{
+		numbers[index] = index + 1;
+		cout << numbers[index] << " | ";
+	}
+
+	cout << endl << "Enter number: ";
+	int key = 0;
+	cin >> key;
+
+	int index = searchBinary(numbers, 0, SIZE, key);
+
+	if (index >= 0)
+	{
+		cout << "Number index: " << index << endl;
+	}
+	else
+	{
+		cout << "No such number in array" << endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
